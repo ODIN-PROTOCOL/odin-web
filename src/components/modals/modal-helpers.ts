d78c64dfda7bb6985d7d2941cd85886cec9864b2
Maksym Nesterov <maxym.nes.dev@gmail.com>
2021-05-13 19:26:07 +0300

    Do not warn on missing dialog handler

--- src/components/modals/modal-helpers.ts
+++ src/components/modals/modal-helpers.ts
@@ -2,5 +2,5 @@ import { DialogEventHandler } from '@/helpers/dialogs'
 import { inject } from 'vue'
 
 export function injectDialogHandler(name: string): DialogEventHandler {
-  return inject(name) || (() => console.warn(`Missing ${name} handler`))
+  return inject(name, () => console.warn(`Missing ${name} handler`), false)
 }
