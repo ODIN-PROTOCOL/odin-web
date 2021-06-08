394d9b7937dcb9c6e6b11a34109362d39e8a6dc8
GeoDB dev <dev@geodb.com>
2021-06-08 16:01:06 +0200

    firebase hosting
    Signed-off-by: GeoDB dev <dev@geodb.com>

--- src/api/api-config.ts
+++ src/api/api-config.ts
@@ -5,9 +5,9 @@ export const API_CONFIG = {
     'prize weapon virus foot mandate finger add strong world physical token castle knee bracket armed quit brand physical neglect film tip okay view broken',
   // api: 'http://35.195.150.105:1317',
   // rpc: 'http://35.195.150.105:26657',
-  rpc: 'http://localhost:26657',
-  faucet: 'http://localhost:5005',
-  exBridge: 'http://localhost:90',
+  rpc: 'https://node.testnet.odinprotocol.io/a/',
+  faucet: 'https://node.testnet.odinprotocol.io/faucet/',
+  exBridge: 'https://node.testnet.odinprotocol.io/bridge/',
   exBridgeFee: '10',
   hdDeviation: stringToPath("m/44'/118'/0'/0/0"),
 }
