3096feb16538567e494860e1997afb92eaa9a731
Sergey-Voskresenskiy <work.sergey.voskeresnskiy@gmail.com>
2021-07-22 17:50:28 +0200

    TEMP: DataSourcePage

--- src/views/DataSources.vue
+++ src/views/DataSources.vue
@@ -1,28 +1,36 @@
 <template>
   <div class="data-sources view-main">
     <div class="fx-row mg-b32">
-      <h2 class="view-title">Data sources</h2>
-      <button
-        class="app-btn-2nd fx-sae"
-        type="button"
-        @click="createDataSource()"
-      >
+      <h2 class="view-title">Data Sources</h2>
+      <button class="app-btn fx-sae" type="button" @click="createDataSource()">
         Create data source
       </button>
     </div>
+
+    <template v-if="dataSources?.length">
+      <div class="fx-row mg-b32">
+        <p>{{ dataSources.length }} data sources found</p>
+      </div>
+    </template>
+
+    <div class="app-table__controls mg-b32">
+      <div class="app-table__search">Search</div>
+      <div class="app-table__sort">
+        <div class="app-table__sort-by">Sort by</div>
+        <div class="app-table__sort-source">Data Source</div>
+      </div>
+    </div>
+
     <div class="app-table">
       <div class="data-sources__table-head app-table__head">
-        <div class="app-table__cell">
-          <span class="app-table__cell-txt"> ID </span>
-        </div>
-        <div class="app-table__cell">
-          <span class="app-table__cell-txt"> Name </span>
+        <div class="app-table__cell mg-b20">
+          <span class="app-table__cell-txt"> Data Source </span>
         </div>
         <div class="app-table__cell">
           <span class="app-table__cell-txt"> Description </span>
         </div>
         <div class="app-table__cell">
-          <span class="app-table__cell-txt"> Owner </span>
+          <span class="app-table__cell-txt"> Timestamp </span>
         </div>
       </div>
       <template v-if="dataSources?.length">
@@ -31,12 +39,6 @@
           :key="item.id.toString()"
           class="data-sources__table-row app-table__row"
         >
-          <div class="app-table__cell">
-            <TitledSpan
-              class="app-table__cell-txt"
-              :text="item.id.toString()"
-            />
-          </div>
           <div class="app-table__cell">
             <TitledSpan class="app-table__cell-txt" :text="item.name" />
           </div>
@@ -92,11 +94,26 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
-.data-sources__table-head,
-.data-sources__table-row {
-  grid:
-    auto /
-    minmax(2rem, 0.1fr) minmax(8rem, 0.5fr) minmax(8rem, 1fr) minmax(8rem, 0.5fr);
+<style lang="scss" scoped>
+.data-sources {
+  &__table-head,
+  &__table-row {
+    grid: auto / minmax(2rem, 1fr) minmax(8rem, 2fr) minmax(1rem, 0.2fr);
+  }
+}
+.app-table {
+  &__controls {
+    font-size: 14px;
+    font-weight: 300;
+    line-height: 2rem;
+  }
+  &__controls,
+  &__sort {
+    display: flex;
+    justify-content: space-between;
+  }
+  &__sort {
+    gap: 2.4rem;
+  }
 }
 </style>
