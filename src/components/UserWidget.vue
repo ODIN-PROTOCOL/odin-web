7d0d2ae249009b3f9ea7f4e02473d0064312e0c2
Sergey-Voskresenskiy <work.sergey.voskeresnskiy@gmail.com>
2021-08-13 14:58:42 +0300

    temp [rvw7yt]

--- src/components/UserWidget.vue
+++ src/components/UserWidget.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="user-widget fx-row gap-5">
     <BalanceButton />
-    <button
-      class="app-btn-2nd w-full"
-      type="submit"
-      @click.prevent="connectMetaMask"
-    >
-      Connect MetaMask
+    <button class="metamask-btn" type="submit" @click.prevent="connectMetaMask">
+      <img src="~@/assets/brand/metamask-fox.svg" alt="metamask_logo" />
+      <span>MetaMask</span>
     </button>
     <button class="app-ico-btn mg-l8" type="button" @click="logOutAndLeave()">
       <img src="~@/assets/icons/exit.svg" alt="Log out" />
@@ -38,4 +35,28 @@ export default defineComponent({
 })
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.metamask-btn {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  padding: 0.4rem 0.8rem;
+  border-radius: 1.2rem;
+  box-shadow: 0 0 1rem 1rem transparent;
+  border: 1px solid var(--clr__action);
+  transition: box-shadow 0.4s ease, color 0.4s ease, background-color 0.4s ease;
+  img {
+    width: 3rem;
+    height: 3rem;
+  }
+  span {
+    line-height: 1;
+    padding-top: 0.2rem;
+  }
+  &:hover {
+    background-color: var(--clr__action-extra-muted);
+    box-shadow: 0 0 1rem 1rem var(--clr__action-extra-muted);
+    color: var(--clr__action);
+  }
+}
+</style>
--- src/components/modals/MetaMaskModal.vue
+++ src/components/modals/MetaMaskModal.vue
@@ -1,36 +1,60 @@
 <template>
   <ModalBase class="exchange-form-modal" @close="onClose()">
     <template #title>
-      <h3>MetaMask | balance: {{ balance }}</h3>
+      <img src="~@/assets/brand/metamask-fox.svg" alt="metamask_logo" />
     </template>
 
     <template #main>
       <form
-        class="app-form load-fog"
+        class="app-form load-fog metamask-form"
         :class="{ 'load-fog_show': isLoading }"
         @submit.prevent
       >
-        <div class="app-form__field">
-          <label class="app-form__field-lbl"> Asset </label>
-          <input
-            class="app-form__field-input"
-            type="text"
-            v-model="form.asset"
-          />
-          <p v-if="form.assetErr" class="app-form__field-err">
-            {{ form.askCountErr }}
-          </p>
-        </div>
-        <div class="app-form__footer">
-          <button
-            class="app-btn"
-            type="button"
-            @click="connectMetaMask()"
-            :disabled="!form.isValid || isLoading"
-          >
-            Connect to MetaMask
-          </button>
-        </div>
+        <template v-if="!account || needAuth.value">
+          <div class="app-form__field">
+            <button
+              class="app-btn"
+              type="button"
+              @click="connectMetaMask()"
+              :disabled="!form.isValid || isLoading"
+            >
+              Connect to MetaMask
+            </button>
+          </div>
+        </template>
+        <template v-else>
+          <div class="app-form__field" v-if="balance">
+            <label class="app-form__field-lbl"> You Balance </label>
+            <input
+              class="app-form__field-input app-form__field-input--disabled"
+              type="text"
+              v-model="balanceBigFromPrecise"
+              disabled
+            />
+          </div>
+          <div class="app-form__field">
+            <label class="app-form__field-lbl"> Amount </label>
+            <input
+              class="app-form__field-input"
+              type="number"
+              v-model="form.amount"
+            />
+            <p class="app-form__field-lbl" style="padding: 1rem 0; font-size: 1rem; color: gray">form.amount: {{ form.amount }}</p>
+            <p v-if="form.amountErr" class="app-form__field-err">
+              {{ form.amountErr }}
+            </p>
+          </div>
+          <div class="app-form__footer">
+            <button
+              class="app-btn"
+              type="button"
+              @click="getExchange"
+              :disabled="!form.isValid || isLoading"
+            >
+              Exchange
+            </button>
+          </div>
+        </template>
       </form>
     </template>
   </ModalBase>
@@ -51,10 +75,18 @@ import { bigFromPrecise } from '@/helpers/bigMath'
 const MetaMaskFormModal = defineComponent({
   name: 'MetaMaskModal',
   components: { ModalBase },
-  setup(_, { emit }) {
-    const assets: Array<string> = ['LOKI']
+  setup() {
+    const needAuth = ref<boolean>(false)
+    const account = ref<string | null>(null)
+    const balance = ref<string | null>()
+    const balanceBigFromPrecise = ref<string | null>()
+
     const form = useForm({
-      asset: [assets[0], validators.required, validators.oneOf(assets)],
+      amount: [
+        0,
+        validators.required,
+        ...validators.num(0, Number(balanceBigFromPrecise)),
+      ],
     })
 
     const isLoading = ref<boolean>(false)
@@ -65,34 +97,47 @@ const MetaMaskFormModal = defineComponent({
       isLoading
     )
 
-    const account = ref<string | null>(null)
-
-    const balance = ref<string>()
-
     // await contracts.odin.methods.approve()
+    // contracts
     const { web3, contracts } = useWeb3({
       onAccountConnected: async (acc): Promise<void> => {
         account.value = acc
-
-        console.log(account.value)
-
-        // await contracts.odin.methods.balanceOf(account.value).call()
-        // console.log(contracts)
-        // bigFromPrecise(
-        //   await contracts.odin.methods.balanceOf(account.value).call(),
-        //   Number(await contracts.odin.methods.decimals().call())
-        // ).toString()
+        if (account.value) await getBalance()
+        console.log('onAccountConnected')
       },
       onAccountDisconnected: () => {
         account.value = null
+        balance.value = null
         console.log('onAccountDisconnected')
       },
     })
 
-    const connectMetaMask = () => {
-      emit('reloadTab')
-      console.log(1)
-      web3.eth.requestAccounts()
+    const getExchange = async (): Promise<void> => {
+      isLoading.value = true
+      console.log(
+        await contracts.odin.methods
+          .approve(account.value as string, form.amount.val())
+          .call()
+      )
+      isLoading.value = false
+    }
+
+    const getBalance = async (): Promise<void> => {
+      balance.value = await contracts.odin.methods
+        .balanceOf(account.value as string)
+        .call()
+
+      const temp = bigFromPrecise(
+        Number(balance.value),
+        Number(await contracts.odin.methods.decimals().call())
+      ).toString()
+
+      balanceBigFromPrecise.value = temp
+      if (temp) isLoading.value = false
+    }
+
+    const connectMetaMask = async () => {
+      await web3.eth.requestAccounts()
     }
 
     const submit = async (): Promise<void> => {
@@ -112,9 +157,17 @@ const MetaMaskFormModal = defineComponent({
     }
     isLoading.value = false
 
-    onMounted(() => {
-      console.log(123)
-      web3.eth.requestAccounts()
+    onMounted(async (): Promise<void> => {
+      isLoading.value = true
+      if ((window as any).ethereum) {
+        const accounts = await web3.eth.getAccounts()
+        if (accounts.length <= 0) {
+          needAuth.value = true
+        } else {
+          needAuth.value = false
+          await getBalance()
+        }
+      }
     })
 
     return {
@@ -125,6 +178,9 @@ const MetaMaskFormModal = defineComponent({
       connectMetaMask,
       account,
       balance,
+      balanceBigFromPrecise,
+      needAuth,
+      getExchange,
     }
   },
 })
@@ -137,3 +193,9 @@ export function showMetaMaskFormDialog(callbacks?: {
   return dialogs.show(MetaMaskFormModal, callbacks)
 }
 </script>
+
+<style lang="scss">
+.metamask-form {
+  margin-top: 3rem;
+}
+</style>
