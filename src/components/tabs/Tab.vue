37f93c0863d592f8dbcf27e68e95ee4cbd72ff86
Mikhail-H <mikhailgorlach@gmail.com>
2021-10-08 11:30:55 +0300

    add tabs component

---
+++ src/components/tabs/Tab.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tab-content" v-show="title == selectedTitle">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from 'vue'
+
+export default defineComponent({
+  props: ['title'],
+  setup: function () {
+    const selectedTitle = inject('selectedTitle')
+
+    return { selectedTitle }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
