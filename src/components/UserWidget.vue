55f746d40b7e26f710dfa7604db3449c92571e7e
Mikhail-H <mikhailgorlach@gmail.com>
2021-10-08 11:34:14 +0300

    add metamask button

--- src/components/UserWidget.vue
+++ src/components/UserWidget.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="user-widget fx-row">
     <Search />
+    <button class="nav-btn">
+      <img src="~@/assets/icons/metamask.svg" alt="metamask" />
+    </button>
     <BalanceButton />
     <button
       class="app-ico-btn log-out-and-leave"
@@ -37,6 +40,34 @@ export default defineComponent({
 .user-widget {
   gap: 2.4rem;
 }
+
+.nav-btn {
+  padding: 0.8rem;
+  width: 4.8rem;
+  height: 4.8rem;
+  border: 1px solid var(--clr__action);
+  border-radius: 4px;
+
+  &:hover {
+    background: var(--clr__action);
+  }
+
+  &:focus {
+    border: 1px solid var(--clr__btn-normal);
+    background: var(--clr__btn-hover);
+  }
+
+  &:hover {
+    border: 1px solid transparent;
+    background: var(--clr__action);
+  }
+
+  &:active {
+    border: 1px solid transparent;
+    background: var(--clr__btn-pressed);
+  }
+}
+
 @media (max-width: 768px) {
   .log-out-and-leave {
     display: none;
