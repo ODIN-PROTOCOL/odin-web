aed7823b3218adde7144080807cc6fa0b03aecf4
Mikhail-H <mikhailgorlach@gmail.com>
2021-10-18 02:05:12 +0300

    create wallet page

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,3 +1,45 @@
+import { getDateFromMessage } from '@/helpers/decodeMessage'
+import { toHex } from '@cosmjs/encoding'
+import { adjustedData } from '@/helpers/Types'
+import { TxResponse } from '@cosmjs/tendermint-rpc/build/tendermint34/responses'
+
+export const _allowedTypes = [
+  'Send',
+  'Receive',
+  'Report',
+  'Request',
+  'Delegate',
+  'Undelegate',
+  'Redelegate',
+  'Withdraw',
+]
+
+export const toHexFunc: (data: Uint8Array) => string = toHex
+
 export const copyValue = (text: string): void => {
   window.navigator.clipboard.writeText(text)
+}
+
+export const prepareTransaction = async (
+  txs: readonly TxResponse[]
+): Promise<Array<adjustedData>> => {
+  let tempArr: Array<adjustedData> = []
+  for (const tx of txs) {
+    const { receiver, sender, type, amount, time, fee } =
+      await getDateFromMessage(tx)
+    tempArr = [
+      ...tempArr,
+      {
+        type: type ? type : '-',
+        hash: tx.hash ? toHexFunc(tx.hash) : '-',
+        block: tx.height ? tx.height : '-',
+        time: time ? time : null,
+        sender: sender ? sender : '',
+        receiver: receiver ? receiver : '',
+        amount: amount ? amount : '',
+        fee: fee ? fee : '-',
+      },
+    ].filter((item) => _allowedTypes.includes(item.type))
+  }
+  return tempArr
 }
