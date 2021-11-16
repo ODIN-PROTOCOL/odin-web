a4967e38a85e2a6426f89b2a9b473539eb387e0a
Mikhail-H <mikhailgorlach@gmail.com>
2021-11-16 14:20:26 +0200

    fix wallet page

--- src/api/api-config.ts
+++ src/api/api-config.ts
@@ -1,8 +1,10 @@
 import { stringToPath } from '@cosmjs/crypto'
 
 export const API_CONFIG = {
-  api: process.env.VUE_APP_API_URL as string,
-  rpc: process.env.VUE_APP_RPC_URL as string,
+  rpc: 'http://192.168.1.37:26657',
+  api: 'http://192.168.1.37:1317',
+  // api: process.env.VUE_APP_API_URL as string,
+  // rpc: process.env.VUE_APP_RPC_URL as string,
   faucet: process.env.VUE_APP_FAUCET_URL as string,
   exBridge: process.env.VUE_APP_EX_BRIDGE_URL as string,
   exBridgeFee: process.env.VUE_APP_EX_BRIDGE_FEE as string,
