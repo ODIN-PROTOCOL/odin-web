07f444d5a957d5051e44690c4c460dd0ebf30858
Mikhail-H <mikhailgorlach@gmail.com>
2021-12-20 10:28:56 +0200

    change chain-config & change hdDeviation

--- src/api/api-config.ts
+++ src/api/api-config.ts
@@ -13,8 +13,8 @@ export const API_CONFIG = {
 
 // Configuration of the chain to which the Kepler extension connects
 export const CHAIN_CONFIG = {
-  chainId: 'odin-testnet-heimdall',
-  chainName: 'odin testnet heimdall',
+  chainId: 'odin-mainnet-freya',
+  chainName: 'odin mainnet freya',
   rpc: API_CONFIG.rpc,
   rest: API_CONFIG.api,
   bip44: {
